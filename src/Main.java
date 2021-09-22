7c48dcde19303f7a502b44567f4d0a1b2fa1e4c3
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-09-22 15:21:25 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int startBalance = 100;
-        int replenishBalance = 5000;
+        int replenishBalance = 1001;
 
         if (replenishBalance > 1_000) {
-           int bonus = replenishBalance/100;
-            System.out.println(startBalance+bonus);
+            int bonus = replenishBalance / 100;
+            System.out.println(startBalance + bonus);
         } else
-            System.out.println(startBalance+0);
+            System.out.println(startBalance + 0);
     }
 }
